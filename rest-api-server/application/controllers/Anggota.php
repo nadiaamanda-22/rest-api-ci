d959ed94af72bf97e0cc0b94c440b1ecad14a49a
nadiaamanda-22 <nadiafebriantuy@gmail.com>
2024-05-03 20:11:18 +0700

    fix : finishing rest api server

--- rest-api-server/application/controllers/Anggota.php
+++ rest-api-server/application/controllers/Anggota.php
@@ -17,12 +17,95 @@ class Anggota extends RestController
 
     public function index_get()
     {
-        $anggota = $this->am->getData();
-        if ($anggota) {
+        $id = $this->get('id');
+        $anggota = $this->am->getData($id);
+
+        if ($id != null) {
+            if ($anggota != null) {
+                $this->response([
+                    'status' => true,
+                    'data' => $anggota
+                ], RestController::HTTP_OK);
+            } else if ($anggota == null) {
+                $this->response([
+                    'status' => false,
+                    'message' => 'id not found!'
+                ], RestController::HTTP_NOT_FOUND);
+            }
+        } else {
             $this->response([
                 'status' => true,
                 'data' => $anggota
-            ]);
+            ], RestController::HTTP_OK);
+        }
+    }
+
+    public function index_post()
+    {
+        $data = [
+            'no_anggota' => $this->post('no_anggota'),
+            'nama_anggota' => $this->post('nama_anggota'),
+            'jabatan' => $this->post('jabatan')
+        ];
+
+        if ($this->am->addData($data) > 0) {
+            $this->response([
+                'status' => true,
+                'message' => 'data added!'
+            ], RestController::HTTP_OK);
+        }
+    }
+
+    public function index_put()
+    {
+        $id = $this->put('id');
+        $data = [
+            'no_anggota' => $this->put('no_anggota'),
+            'nama_anggota' => $this->put('nama_anggota'),
+            'jabatan' => $this->put('jabatan')
+        ];
+
+        if ($id == null) {
+            $this->response([
+                'status' => false,
+                'message' => 'ID unknow!'
+            ], RestController::HTTP_BAD_REQUEST);
+        } else {
+            if ($this->am->editData($id, $data) > 0) {
+                $this->response([
+                    'status' => true,
+                    'message' => 'updated data!'
+                ], RestController::HTTP_OK);
+            } else {
+                $this->response([
+                    'status' => false,
+                    'message' => 'id not found!'
+                ], RestController::HTTP_NOT_FOUND);
+            }
+        }
+    }
+
+    public function index_delete()
+    {
+        $id = $this->delete('id');
+
+        if ($id == null) {
+            $this->response([
+                'status' => false,
+                'message' => 'ID unknow!'
+            ], RestController::HTTP_BAD_REQUEST);
+        } else {
+            if ($this->am->delData($id) > 0) {
+                $this->response([
+                    'status' => true,
+                    'message' => 'deleted data!'
+                ], RestController::HTTP_OK);
+            } else {
+                $this->response([
+                    'status' => false,
+                    'message' => 'id not found!'
+                ], RestController::HTTP_NOT_FOUND);
+            }
         }
     }
 }
--- rest-api-server/application/models/anggota_model.php
+++ rest-api-server/application/models/anggota_model.php
@@ -3,8 +3,30 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class anggota_model extends CI_Model
 {
-    public function getData()
+    public function getData($id = null)
     {
-        return $this->db->get('anggota')->result_array();
+        if ($id == null) {
+            return $this->db->get('anggota')->result_array();
+        } else {
+            return $this->db->get_where('anggota', ['id' => $id])->result_array();
+        }
+    }
+
+    public function addData($data)
+    {
+        $this->db->insert('anggota', $data);
+        return $this->db->affected_rows();
+    }
+
+    public function editData($id, $data)
+    {
+        $this->db->update('anggota', $data, ['id' => $id]);
+        return $this->db->affected_rows();
+    }
+
+    public function delData($id)
+    {
+        $this->db->delete('anggota', ['id' => $id]);
+        return $this->db->affected_rows();
     }
 }
